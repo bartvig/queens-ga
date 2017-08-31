33823adadec86ba52a31890afeb6c6bfa1fb0dab
Morten Bartvig <bartvig@gmail.com>
2017-08-31 12:39:48 +0200

    Added evaluate and to_string functions.

--- src/QueensGenome.php
+++ src/QueensGenome.php
@@ -34,4 +34,73 @@ class QueensGenome extends Genome {
     // TODO: Implement mutate() method.
   }
 
+  /**
+   * QOD fitness evaluation function.
+   */
+  public function evaluate() {
+    $fitness = 0;
+    $rows = [];
+    $column = 0;
+    $diagonals_1 = [];
+    $diagonals_2 = [];
+    foreach ($this->genome as $gene) {
+      // Use 0 as lowest value for gene.
+      $gene--;
+      if (!isset($rows[$gene])) {
+        $rows[$gene] = 0;
+      }
+      else {
+        $rows[$gene]++;
+      }
+
+      // Find diagonals.
+      $diagonal = $column + $gene;
+      if (!isset($diagonals_1[$diagonal])) {
+        $diagonals_1[$diagonal] = 0;
+      }
+      else {
+        $diagonals_1[$diagonal]++;
+      }
+
+      $diagonal = 7 - $column + $gene;
+      if (!isset($diagonals_2[$diagonal])) {
+        $diagonals_2[$diagonal] = 0;
+      }
+      else {
+        $diagonals_2[$diagonal]++;
+      }
+
+      $column++;
+    }
+
+    foreach ($rows as $val) {
+      $fitness += $val;
+    }
+    foreach ($diagonals_1 as $val) {
+      $fitness += $val;
+    }
+    foreach ($diagonals_2 as $val) {
+      $fitness += $val;
+    }
+
+    $this->fitness = $fitness;
+  }
+
+  public function toString() {
+    $output = ['', '', '', '', '', '', '', ''];
+
+    for ($column = 0; $column < 8; $column++) {
+      for ($row = 0; $row < 8; $row++) {
+        if ($this->genome[$column]-1 == $row) {
+          $output[$row] .= 'Q';
+        }
+        else {
+          $output[$row] .= '.';
+        }
+      }
+    }
+
+    return join ("\n", $output) . "\n";
+  }
+
 }
