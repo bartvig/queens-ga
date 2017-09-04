bedd200da58a0bbfdf81eac2abd4f869317912fa
Morten Bartvig <bartvig@gmail.com>
2017-09-04 16:22:13 +0200

    Use islands instead of population.

--- queens.php
+++ queens.php
@@ -1,9 +1,8 @@
 <?php
 
-require __DIR__ . '/../simple-ga/vendor/autoload.php';
 require __DIR__ . '/vendor/autoload.php';
 
-use \SimpleGA\Population;
+use \SimpleGA\Islands;
 
 $container = new \Pimple\Container();
 
@@ -14,30 +13,36 @@ $container['random_generator'] = function ($c) {
   };
 };
 $container['genome'] = $container->factory(function ($c) {
-  return new \QueensGA\QueensGenome($c['random_generator']);
+  return new \QueensGA\QueensGenome($c);
 });
 
-$container['population_size'] = 1000;
-$container['population_random'] = 0;
+$container['population_size'] = 20;
+$container['population_random'] = 1;
 $container['elite_count'] = 1;
 $container['mutation_promille'] = 1;
+$container['islands_count'] = 10;
+$container['islands_exchange_count'] = 10;
+$container['islands_exchange_generations'] = 20;
 
 $generations = 10000;
 $shortcut_fitness = 0;
 
-$population = new Population($container);
-$population->initialize();
+$islands = new Islands($container);
+$islands->initialize();
 
-$fittest = $population->getFittestGenome();
+$fittest = $islands->getFittestGenome();
 print "Initial fittest genome:\n";
 print "Fitness: " . $fittest->getFitness() . "\n";
 print $fittest->toString();
 
 $best_fitness = -1;
-while ($population->nextGeneration() <= $generations && $best_fitness != $shortcut_fitness) {
-  print "Generation " . $population->getGeneration() . "\n";
+while ($islands->nextGeneration() <= $generations && $best_fitness != $shortcut_fitness) {
+  print "Generation " . $islands->getGeneration() . "\n";
+  if ($islands->getGeneration() % $container['islands_exchange_generations'] == 0) {
+    print "Exchanging genomes between islands.";
+  }
   print "Most fit genome:\n";
-  $fittest = $population->getFittestGenome();
+  $fittest = $islands->getFittestGenome();
   $best_fitness = $fittest->getFitness();
   print "Fitness: " . $fittest->getFitness() . "\n";
   print $fittest->toString() . "\n";
